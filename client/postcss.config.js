b15c6b6162c408ea3a887fdd80e9b2dfac4cf6d5
btwtejash-design <btw.tejash@gmail.com>
2025-11-02 15:22:31 +0530

    fixed deployment error

--- client/postcss.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {},  // Add this (or replace 'tailwindcss': {} if it exists)
-    autoprefixer: {},  // Keep this if you have it for browser compatibility
-  },
-}
